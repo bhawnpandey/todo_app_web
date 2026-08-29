5141e5412ca3a29cdcd8bd8e69b257276cf322a5
Bhawna Pandey <bhawnapandey@Bhawnas-MacBook-Pro.local>
2026-08-29 12:13:07 +0200

    working with streamlit

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ st.subheader("This is my todo app")
 
 todos = functions.get_todos()
 
-for todo in todos:
-    st.checkbox(todo)
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+for index, todo in enumerate(todos):
+    checkbox_value = st.checkbox(todo, key=todo)
+    if checkbox_value:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Add new todo..", key="new_todo", on_change=add_todo)
+
+
+
 
 
